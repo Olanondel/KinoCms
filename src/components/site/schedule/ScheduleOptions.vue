703e04e52355a2757d63671b040d141321354989
Deqpwtf <prbaoleg@gmail.com>
2021-09-25 18:23:22 +0300

    added search for films and filters on schedule page

---
+++ src/components/site/schedule/ScheduleOptions.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-group" v-if="options.length">
+    <select v-model="value" @input="$emit('input', $event.target.value)" class="form-control">
+      <option disabled value>{{text}}</option>
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+      >{{option}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: Array,
+    value: String,
+    text: String
+  },
+  name: "BaseOptions",
+  data() {
+    return {
+      select: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
